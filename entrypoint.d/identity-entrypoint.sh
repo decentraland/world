d723d66287fde8aa2304d84e79dbbb3f484b2fa8
Mario Sossich <m-sossich@users.noreply.github.com>
2019-05-08 17:38:15 -0300

    chore: entrypoint (#22)
    * chore: entrypoint
    
    * chore: entrypoint

---
+++ entrypoint.d/identity-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+FILE="config/enabledClients.json"
+
+echo "Overwriting the file ${FILE}"
+echo -e ${CONFIG_SERVICE_JSON} | sed 's/\\//g' > ${FILE}
+cat ${FILE}
+
+echo "Setting private key..."
+echo -e ${PRIV_KEY} | sed 's/\\//g' > ${PRIV_KEY_PATH}
+
+if [ -s ${PRIV_KEY_PATH} ]
+then
+     echo "...Done"
+else
+     echo "...Failed to write private key"
+     exit 1
+fi
+
+exec ./build/identity $@
