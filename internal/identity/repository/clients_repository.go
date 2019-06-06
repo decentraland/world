f961ab1844d0650270f9a7bde5391ae17d6d7bfe
Mario S <mario@decentraland.org>
2019-06-06 14:23:13 -0300

    fix: load error

--- internal/identity/repository/clients_repository.go
+++ internal/identity/repository/clients_repository.go
@@ -42,8 +42,8 @@ type ClientRepository interface {
 }
 
 type clientRepoImpl struct {
-	idIndex     map[string]*ClientData
-	domainIndex map[string]*ClientData
+	idIndex     map[string]ClientData
+	domainIndex map[string]ClientData
 }
 
 func (c *clientRepoImpl) GetById(clientId string) (*ClientData, error) {
@@ -54,14 +54,14 @@ func (c *clientRepoImpl) GetByDomain(domain string) (*ClientData, error) {
 	return doQuery(domain, c.domainIndex)
 }
 
-func doQuery(key string, index map[string]*ClientData) (*ClientData, error) {
+func doQuery(key string, index map[string]ClientData) (*ClientData, error) {
 	client, ok := index[key]
 
 	if !ok {
 		return nil, errors.New("client not found")
 	}
 
-	return client, nil
+	return &client, nil
 }
 
 func NewClientRepository(dataPath string) (ClientRepository, error) {
@@ -71,19 +71,19 @@ func NewClientRepository(dataPath string) (ClientRepository, error) {
 		log.WithError(err).Errorf("Failed to read client data from file: %s", dataPath)
 		return nil, err
 	}
-	ids := make(map[string]*ClientData)
-	domains := make(map[string]*ClientData)
+	ids := make(map[string]ClientData)
+	domains := make(map[string]ClientData)
 
 	for _, c := range data {
 		log.Debugf("Loading data for Domain: %s  Id: %s", c.Domain, c.Id)
 		if _, ok := ids[c.Id]; !ok {
-			ids[c.Id] = &c
+			ids[c.Id] = c
 		} else {
 			log.Warnf("Duplicate ClientID: %s", c.Id)
 		}
 
 		if _, ok := ids[c.Domain]; !ok {
-			domains[c.Domain] = &c
+			domains[c.Domain] = c
 		} else {
 			log.Warnf("Duplicate Domain: %s", c.Id)
 		}
