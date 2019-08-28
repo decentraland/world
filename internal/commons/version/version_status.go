44458b3fa26c5b43ab38f647eacb70c7337f8708
Hugo Arregui <hugo@decentraland.org>
2019-08-28 10:23:15 -0300

    add version endpoint for comm server and coordinator

--- internal/commons/version/version_status.go
+++ internal/commons/version/version_status.go
@@ -1,22 +1,9 @@
 package version
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 var version = "Not available"
 
 type versionResponse struct {
 	Version string `json:"version"`
-}
-
-func RegisterVersionEndpoint(r gin.IRoutes) {
-	v := &versionResponse{Version: version}
-	r.GET("/version", func(c *gin.Context) {
-		c.JSON(http.StatusOK, v)
-	})
 }
 
 func Version() string {
